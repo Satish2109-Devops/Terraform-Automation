2a87daafea9b733b9dff341dea04346f78785860
Satish  Mishra <satmish2109@gmail.com>
2025-12-28 17:44:03 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state"
+    bucket = "young-minds-app-project-1-terraform-state"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
