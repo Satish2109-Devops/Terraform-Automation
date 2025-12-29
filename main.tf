81736f5eee7abf8e750fa9d5c2ed48fb639a80c5
Satish Mishra <satishmishra7489@gmail.com>
2025-12-30 01:08:45 +0530

    all code modified/updated  REQ112211

--- main.tf
+++ main.tf
@@ -2,18 +2,17 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "172.16.0.0/16"
-  instance_tenancy = "default"
-  tags = {
-    Name = "main"
-  }
-}
-
-#Create security group with firewall rules
-resource "aws_security_group" "jenkins-sg-2022" {
+# Security Group in default VPC
+resource "aws_security_group" "jenkins_sg" {
   name        = var.security_group
-  description = "security group for Ec2 instance"
+  description = "security group for EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 8080
@@ -22,14 +21,6 @@ resource "aws_security_group" "jenkins-sg-2022" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
   egress {
     from_port   = 0
     to_port     = 65535
@@ -37,57 +28,57 @@ resource "aws_security_group" "jenkins-sg-2022" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags= {
+  tags = {
     Name = var.security_group
   }
 }
 
+# First EC2 Instance in default VPC
 resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
-
-  # Set root volume size to 20 GB
-  root_block_device {
-    volume_size = 20
-    volume_type = "gp3"
-    delete_on_termination = true
-  }
-
-  tags= {
-    Name = var.tag_name
-  }
-}
-
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
- // vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
-
-# Second EC2 Instance
-resource "aws_instance" "mySecondInstance" {
   ami                    = var.ami_id
   key_name               = var.key_name
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
- tags= {
-    Name = "${var.tag_name}-2"
-  }
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
   root_block_device {
     volume_size           = 20
     volume_type           = "gp3"
     delete_on_termination = true
   }
 
-  
+  tags = {
+    Name = var.tag_name
+  }
 }
 
-# Elastic IP for Second Instance
+# Elastic IP for first instance
+resource "aws_eip" "myFirstInstance" {
+  instance = aws_instance.myFirstInstance.id
+
+  tags = {
+    Name = "my_elastic_ip"
+  }
+}
+
+# Second EC2 Instance in default VPC
+resource "aws_instance" "mySecondInstance" {
+  ami                    = var.ami_id
+  key_name               = var.key_name
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  root_block_device {
+    volume_size           = 20
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "${var.tag_name}-2"
+  }
+}
+
+# Elastic IP for second instance
 resource "aws_eip" "mySecondInstance" {
   instance = aws_instance.mySecondInstance.id
 
@@ -95,5 +86,3 @@ resource "aws_eip" "mySecondInstance" {
     Name = "my_elastic_ip_2"
   }
 }
-
-
